cde6ec83c02117656c6da7432fd5f1f0bfa6f9a8
vimleshxrpaswan <paswanvimlesh481@gmail.com>
2023-05-30 16:29:42 +0530

    removed comment

--- src/leetcodeprac/Solution.java
+++ src/leetcodeprac/Solution.java
@@ -1,9 +1,9 @@
 package leetcodeprac;
 
-import java.util.Arrays;
+
 
 public class Solution {
-//    static int[][] ar1;
+
 
     public static int[] twoSum(int[] numbs, int target) {
         int[] ar2;
@@ -12,9 +12,6 @@ public class Solution {
                 if (target == numbs[i] + numbs[j]) {
 
                     return new int[]{i, j};
-//                    ar2 = new int[]{i, j};
-//                    ar1 = new int[][]{ar2};
-//                    System.out.println("ar1 : "+ Arrays.toString(ar2));
                 }
             }
         }
@@ -25,8 +22,6 @@ public class Solution {
         int[] numbs = {1, 2, 3, 4, 5};
         int target = 5;
         int[] ints = twoSum(numbs, target);
-//        List<Integer> collect = Arrays.stream(ints).flatMap(Arrays::stream).collect(Collectors.toList());
-
         System.out.println(ints);
 
     }
