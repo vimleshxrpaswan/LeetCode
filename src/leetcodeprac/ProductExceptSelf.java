827b7f03d205bef061e3940cada9d0f14c0b5cc2
vimleshxrpaswan <paswanvimlesh481@gmail.com>
2023-06-29 14:13:15 +0530

    replacing each element with the product of every other element without using the division operator

---
+++ src/leetcodeprac/ProductExceptSelf.java
@@ -0,0 +1,23 @@
+package leetcodeprac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductExceptSelf {
+        public static void main(String[] args) {
+            int[]  arr= {1, 2, 3, 4, 5};
+            List<Integer> resultList = new ArrayList<>();
+            int resultValue = 1;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr.length; j++) {
+                    if (i == j)
+                        continue;
+                    resultValue *= arr[j];
+                }
+                resultList.add(resultValue);
+                resultValue = 1;
+            }
+            System.out.println(resultList);
+        }
+    }
+
